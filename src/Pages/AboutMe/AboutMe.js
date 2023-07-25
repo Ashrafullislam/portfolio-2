9d7f2a4dec9d731000970a719a7c49d82af99aab
Ashrafullislam <ashrafull130@gmail.com>
2023-07-25 08:42:13 +0600

    added updated cv resume link

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -50,11 +50,11 @@ const AboutMe = () => {
       <h2 className='flex items-center gap-2 mt-2'  > <FaPhoneAlt className='text-warning  ' /> +88 01778 231817 </h2>
     <div className="card-actions lg:justify-end mt-4">
     <div >
-          <a href='https://drive.google.com/file/d/1kIgGMlRtY5bi3zlpWopgKJezwZZDgy-Q/view?usp=share_link'> <button className='btn  bg-gradient-to-r from-warning to-primary border-none shadow text-slate-200 hover:bg-gradient-to-r hover:from-primary hover:to-warning  ' > My CV  <FaArrowRight className='text-xl text-warning ml-3 ' /> </button> </a>
+          <a href='https://drive.google.com/file/d/1RtVTDIJFo4vNmyvEeeCk1fmPJhDrCK6a/view?usp=sharing'> <button className='btn  bg-gradient-to-r from-warning to-primary border-none shadow text-slate-200 hover:bg-gradient-to-r hover:from-primary hover:to-warning  ' > My CV  <FaArrowRight className='text-xl text-warning ml-3 ' /> </button> </a>
         </div>
         
     <div >
-          <a href="https://drive.google.com/file/d/1YyFtHDR_23-DhD2l7oGuF5_9Tg0vANNS/view?usp=share_link"> <button className='btn  bg-gradient-to-r from-warning to-primary border-none shadow text-slate-200 hover:bg-gradient-to-r hover:from-primary hover:to-warning  ' > Get My Resume <FaArrowRight className='text-xl text-warning ml-3 ' /> </button> </a>
+          <a href="https://drive.google.com/file/d/1wtYR2ImsnuM4O99HfxkAeiPslW6ED7SM/view?usp=sharing"> <button className='btn  bg-gradient-to-r from-warning to-primary border-none shadow text-slate-200 hover:bg-gradient-to-r hover:from-primary hover:to-warning  ' > Get My Resume <FaArrowRight className='text-xl text-warning ml-3 ' /> </button> </a>
         </div>
 
        
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -183,7 +183,7 @@ const Banner = () => {
                 </div>
                
                       <div className='lg:ml-4 mt-2 ' >
-                      <a href="https://drive.google.com/file/d/1YyFtHDR_23-DhD2l7oGuF5_9Tg0vANNS/view?usp=share_link"> <button className='btn  bg-gradient-to-r from-warning to-primary border-none shadow text-slate-200 hover:bg-gradient-to-r hover:from-primary hover:to-warning ' > Get My Resume <FaArrowRight className='text-xl text-warning ml-3 ' /> </button> </a>
+                      <a href="https://drive.google.com/file/d/1wtYR2ImsnuM4O99HfxkAeiPslW6ED7SM/view?usp=sharing"> <button className='btn  bg-gradient-to-r from-warning to-primary border-none shadow text-slate-200 hover:bg-gradient-to-r hover:from-primary hover:to-warning ' > Get My Resume <FaArrowRight className='text-xl text-warning ml-3 ' /> </button> </a>
                         </div> 
             </div>
 
